0d27acde02b34b1698afce8ff696d133ff4a6556
toehead2001 <toe_head2001@yahoo.com>
2015-10-06 23:17:47 -0600

    Added Font Styles

--- TextWindow.cs
+++ TextWindow.cs
@@ -13,6 +13,10 @@ int Amount3 = 12; // [6,250] Font Size
 FontFamily Amount4 = new FontFamily("Arial"); // Font
 Pair<double, double> Amount5 = Pair.Create(0.0, 0.0); // Offset
 ColorBgra Amount6 = ColorBgra.FromBgr(0, 0, 0); // Background Color
+bool Amount7 = false; // [0,1] Bold
+bool Amount8 = false; // [0,1] Italic
+bool Amount9 = false; // [0,1] Underline
+bool Amount10 = false; // [0,1] Strikeout
 #endregion
 
 void InvalidFontMessage(string msg, string caption)
@@ -22,6 +26,43 @@ void InvalidFontMessage(string msg, string caption)
         {
             System.Windows.Forms.MessageBox.Show(msg, caption);
         }), null);
+}
+
+private FontStyle fontStyles()
+{
+    List<FontStyle> styleList = new List<FontStyle>();
+    if (Amount7)
+        styleList.Add(FontStyle.Bold);
+    if (Amount8)
+        styleList.Add(FontStyle.Italic);
+    if (Amount9)
+        styleList.Add(FontStyle.Underline);
+    if (Amount10)
+        styleList.Add(FontStyle.Strikeout);
+
+    FontStyle styles;
+    switch (styleList.Count)
+    {
+        case 0:
+            styles = FontStyle.Regular;
+            break;
+        case 1:
+            styles = styleList[0];
+            break;
+        case 2:
+            styles = styleList[0] | styleList[1];
+            break;
+        case 3:
+            styles = styleList[0] | styleList[1] | styleList[2];
+            break;
+        case 4:
+            styles = styleList[0] | styleList[1] | styleList[2] | styleList[3];
+            break;
+        default:
+            styles = FontStyle.Regular;
+            break;
+    }
+    return styles;
 }
 
 void Render(Surface dst, Surface src, Rectangle rect)
@@ -38,7 +79,7 @@ void Render(Surface dst, Surface src, Rectangle rect)
     Graphics g = Graphics.FromImage(textBitmap);
 
     RectangleF textRect = new RectangleF((float)Amount5.First * selection.Width, (float)Amount5.Second * selection.Height, selection.Width, selection.Height);
-    Font font = new Font(Amount4, Amount3);
+    Font font = new Font(Amount4, Amount3, fontStyles());
     g.TextRenderingHint = TextRenderingHint.AntiAlias;
 
     string text = Amount1 + " ";
